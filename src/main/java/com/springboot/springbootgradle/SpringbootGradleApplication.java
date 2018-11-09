7cd82b0194371d8077cbe0e786e0ddefb77f8f24
menglin319 <33089669+menglin319@users.noreply.github.com>
2018-11-09 13:33:57 +0800

    springboot-gradle

---
+++ src/main/java/com/springboot/springbootgradle/SpringbootGradleApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.springbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleApplication.class, args);
+	}
+}
